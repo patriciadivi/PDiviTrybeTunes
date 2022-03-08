03614885e25d9d3c9513ab7cea419b1cd736fd6f
PatriciaDivi <patriciavianad@gmail.com>
2022-03-08 14:28:41 -0300

    2 requisito | Crie um formulário para identificação | :rocket: | concluido

---
+++ src/pages/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Loading extends React.Component {
+  render() {
+    return (
+      <section className="Loading">
+        <h1>Carregando... </h1>
+      </section>
+    );
+  }
+}
+
+export default Loading;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,79 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: '',
+      isSaveButtonDisabled: true,
+      loading: false,
+      logado: false,
+    };
+  }
+
+  handlerChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    }, this.validLogin);
+  };
+
+  validLogin = () => {
+    const minlengthValue = 3;
+    const { user } = this.state;
+    const userValid = user.length >= minlengthValue;
+    this.setState({
+      isSaveButtonDisabled: !userValid,
+    });
+  }
+
+  handlerOnclick = async () => {
+    const { user } = this.state;
+    this.setState({
+      loading: true,
+    });
+    await createUser({ name: user });
+    this.setState({
+      logado: true,
+    });
+  }
+
   render() {
+    const { user, isSaveButtonDisabled, loading, logado } = this.state;
     return (
-      <div data-testid="page-login">
-        Login
-      </div>
+      <section>
+        {
+          !loading
+            ? (
+              <div data-testid="page-login">
+                <h2>Login</h2>
+                <input
+                  type="text"
+                  id="user"
+                  name="user"
+                  value={ user }
+                  onChange={ this.handlerChange }
+                  data-testid="login-name-input"
+                />
+                <button
+                  type="submit"
+                  disabled={ isSaveButtonDisabled }
+                  data-testid="login-submit-button"
+                  onClick={ this.handlerOnclick }
+                >
+                  Entrar
+                </button>
+              </div>
+            )
+            : (
+              <div>
+                { logado ? (<Redirect to="/search" />) : (<Loading />) }
+              </div>
+            )
+        }
+      </section>
     );
   }
 }
