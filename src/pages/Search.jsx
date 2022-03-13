0426e1303ed29461029c5c3a1da2202ece6cd618
PatriciaDivi <patriciavianad@gmail.com>
2022-03-13 14:59:26 -0300

    5 Requisito |  Crie o formulário para pesquisar artistas | concluido | :rocket: VQV!

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,64 @@ import React from 'react';
 import Header from '../component/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchValueInput: '',
+      disabledButtom: true,
+    };
+  }
+
+  handleChandeSerach = ({ target: { value, name } }) => {
+    this.setState({
+      [name]: value,
+    }, this.validSearch);
+  }
+
+  validSearch = () => {
+    const numberMin = 2;
+    const { searchValueInput } = this.state;
+    const validNumberMin = searchValueInput.length >= numberMin;
+    this.setState({
+      disabledButtom: !validNumberMin,
+    });
+  }
+
+  handlerOnclickSearch = (event) => {
+    event.preventDefault();
+  }
+
   render() {
+    const { searchValueInput, disabledButtom } = this.state;
     return (
-      <div data-testid="page-search">
-        <Header />
-      </div>
+      <section>
+        <div data-testid="page-search">
+          <Header />
+        </div>
+        <section>
+          <form action="">
+            <label htmlFor="searchId">
+              Nome do artista:
+              <input
+                id="searchId"
+                name="searchValueInput"
+                value={ searchValueInput }
+                type="text"
+                onChange={ this.handleChandeSerach }
+                data-testid="search-artist-input"
+              />
+            </label>
+            <button
+              type="submit"
+              disabled={ disabledButtom }
+              onClick={ this.handlerOnclickSearch }
+              data-testid="search-artist-button"
+            >
+              Pesquisar
+            </button>
+          </form>
+        </section>
+      </section>
     );
   }
 }
