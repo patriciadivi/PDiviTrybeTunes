659bfe599c0a0610bab8f029092add7e7b0fe839
PatriciaDivi <patriciavianad@gmail.com>
2022-03-14 04:01:52 -0300

    8 requisito | Crie o mecanismo para adicionar músicas na lista de músicas favoritas | :rocket: | VQV

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -3,6 +3,7 @@ import PropType from 'prop-types';
 import getMusics from '../services/musicsAPI';
 import Header from '../component/Header';
 import Loading from './Loading';
+import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Album extends React.Component {
   constructor() {
@@ -10,23 +11,57 @@ class Album extends React.Component {
 
     this.state = {
       dataMusics: '',
+      favorite: '',
+      loading: false,
     };
   }
 
   async componentDidMount() {
     const { match } = (this.props);
     const restMusic = await getMusics(match.params.id);
-    this.setState({ dataMusics: restMusic });
+    const resultFavoriteMusic = await getFavoriteSongs();
+    const filterFavoriteId = resultFavoriteMusic
+      .map((musicFavorite) => musicFavorite[0].trackId);
+    this.setState({
+      dataMusics: restMusic,
+      favorite: filterFavoriteId,
+    });
+  }
+
+  checkFavorite = async ({ target }) => {
+    const { id } = target;
+    this.setState({ loading: true });
+    console.log(target.id);
+
+    if (target.checked) {
+      const music = await getMusics(id);
+      await addSong(music);
+      this.setState((prevState) => ({
+        favorite: [...prevState.favorite, id], loading: false,
+      }));
+    } else {
+      const { favorite } = this.state;
+      const musicDelete = favorite.filter((musicFavorite) => musicFavorite !== id);
+      this.setState(() => ({
+        favorite: [musicDelete], loading: false,
+      }));
+    }
+  }
+
+  verifyFavoriteCheck = (id) => {
+    const { favorite } = this.state;
+    const verifyFavorite = favorite.find((musicFavorite) => Number(musicFavorite) === id);
+    return verifyFavorite;
   }
 
   render() {
-    const { dataMusics } = this.state;
+    const { dataMusics, favorite, loading } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
         <p>Album</p>
 
-        {dataMusics.length === 0 ? <Loading />
+        {!favorite ? ''
           : (
             <section>
               <div>
@@ -40,28 +75,42 @@ class Album extends React.Component {
                 </div>
               </div>
 
-              <div>
-                {(
-                  dataMusics.map((mus, index) => (
-                    index === 0 ? ''
-                      : (
-                        <div key={ mus.trackId }>
-                          <p>{mus.trackName}</p>
-                          <audio
-                            data-testid="audio-component"
-                            src={ mus.previewUrl }
-                            controls
-                          >
-                            <track kind="captions" />
-                            O seu navegador não suporta o elemento
-                            {' '}
-                            <code>audio</code>
-                          </audio>
-                        </div>)
-                  ))
+              { loading ? <Loading />
+                : (
+                  <div>
+                    {(
+                      dataMusics.map((mus, index) => (
+                        index === 0 ? ''
+                          : (
+                            <div key={ mus.trackId }>
+                              <p>{mus.trackName}</p>
+                              <audio
+                                data-testid="audio-component"
+                                src={ mus.previewUrl }
+                                controls
+                              >
+                                <track kind="captions" />
+                                O seu navegador não suporta o elemento
+                                {' '}
+                                <code>audio</code>
+                              </audio>
+                              <label htmlFor={ mus.trackId }>
+                                Favorita
+                                <input
+                                  type="checkbox"
+                                  id={ mus.trackId }
+                                  name="favoritaMusic"
+                                  onChange={ this.checkFavorite }
+                                  data-testid={ `checkbox-music-${mus.trackId}` }
+                                  checked={ this.verifyFavoriteCheck(mus.trackId) }
+                                />
+                              </label>
+                            </div>)
+                      ))
+                    )}
+                    ;
+                  </div>
                 )}
-                ;
-              </div>
             </section>)}
       </div>
     );
